e23c4e42ba7a761a9c24c709c8631ff37033124d
Pohigarant1988 <pohigarant@mail.ru>
2026-02-20 22:33:24 +0300

    Dom zadanie

---
+++ dz.py
@@ -0,0 +1,32 @@
+from datetime import datetime, date
+import json
+import pprint
+import os
+
+
+class Loger:
+
+    def __init__(self, msg):
+        self.msg = msg
+
+    def log(self):
+        now = datetime.now()
+        month_year = now.strftime("%m-%Y")
+        day = now.strftime("%d")
+        file_name = f"{now.strftime("%H-%M-%S")}.txt"
+        file_path = os.path.join(month_year, day, file_name)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        massege = {
+            "timestamp": datetime.now().isoformat(),
+            "msg": self.msg,
+        }
+        with open(file_path, "w") as f:
+            json.dump(massege, f)
+
+        with open(file_path, "r") as f:
+            res = json.load(f)
+            pprint.pprint(res)
+
+
+a = Loger("Привет")
+a.log()
--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,15 @@
 from django.conf import settings
 from django.db import models
+from django.utils.text import slugify
 
 
 class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name="Заголовок")
     content = models.TextField(verbose_name="Содержание")
     publish_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата публикации")
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Автор", related_name="posts")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Автор",
+                               related_name="posts")
+    slug = models.SlugField(max_length=200, unique=True, verbose_name="слаг")
 
     class Meta:
         ordering = ['-publish_date']
@@ -15,5 +18,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
