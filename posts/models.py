cba0a18598a18800bbb01f6273ffdf9b14b831c7
Pohigarant1988 <Pohigarant@mail.ru>
2026-02-23 10:50:30 +0300

    first change

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,22 @@
-from tabnanny import verbose
-
 from django.conf import settings
 from django.db import models
 from django.utils.text import slugify
 
+class Cats(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Имя")
+    slug = models.SlugField(max_length=100, unique=True, verbose_name="Слаг")
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Слаг"
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
 class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name="Заголовок")
@@ -11,7 +24,7 @@ class Post(models.Model):
     publish_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата публикации")
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Автор",
                                related_name="posts")
-    categoria = models.ManyToManyField(Categories, on_delete=models.SET_DEFAULT, default="", verbose_name="Категория",
+    categoria = models.ManyToManyField(Cats, blank=True, verbose_name="Категория",
                                        related_name="post_category")
     slug = models.SlugField(max_length=200, unique=True, verbose_name="слаг")
 
@@ -29,18 +42,4 @@ class Post(models.Model):
         super().save(*args, **kwargs)
 
 
-class Categories(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Имя")
-    slug = models.SlugField(max_length=100, unique=True, verbose_name="Слаг")
 
-    class Meta:
-        ordering = ["name"]
-        verbose_name = "Слаг"
-
-    def __str__(self):
-        return self.name
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
