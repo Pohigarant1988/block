7eec6867050f3da0718ff95ff1274cf6d0ea4725
Pohigarant1988 <Pohigarant@mail.ru>
2026-02-24 10:07:20 +0300

    second change

--- posts/models.py
+++ posts/models.py
@@ -2,21 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.utils.text import slugify
 
-class Cats(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Имя")
-    slug = models.SlugField(max_length=100, unique=True, verbose_name="Слаг")
-
-    class Meta:
-        ordering = ["name"]
-        verbose_name = "Слаг"
-
-    def __str__(self):
-        return self.name
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
 
 class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name="Заголовок")
@@ -24,8 +9,9 @@ class Post(models.Model):
     publish_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата публикации")
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Автор",
                                related_name="posts")
-    categoria = models.ForeignKey(Cats, on_delete=models.SET_DEFAULT,default= "", verbose_name="Категория",
-                                       related_name="post_category")
+    cat = models.ForeignKey("Cats", on_delete=models.SET_NULL, null=True, blank=True,
+                            verbose_name="Категория",
+                            related_name="post_category")
     slug = models.SlugField(max_length=200, unique=True, verbose_name="слаг")
 
     class Meta:
@@ -42,4 +28,18 @@ class Post(models.Model):
         super().save(*args, **kwargs)
 
 
+class Cats(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Имя")
+    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name="Слаг")
 
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Слаг"
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
